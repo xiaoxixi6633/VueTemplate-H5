4ef2e44e015c31b17f4e4d0bbb3452f9b8a7f799
ex_mianmianwang@jieyuechina.com <281419583@qq.com>
2018-09-30 16:01:33 +0800

    开发环境

---
+++ src/dc/net.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: xixi
+ * @Date: 2018-06-01 09:30:38
+ * @Last Modified by: xixi
+ * @Last Modified time: 2018-06-06 14:40:40
+ */
+  // const BASE_URL = 'http://192.168.34.183:10000';//开发地址
+//  const BASE_URL = 'http://172.18.101.118:10000';//开发地址（内部开发和测试的地址）
+   const BASE_URL = 'http://172.18.101.111:10000';//测试地址（正式提测的地址）
+  // const BASE_URL = 'http://bi.jieyuechina.com/api'; //正式环境地址, #***###记得修改我的-关于-版本号
+  export default{
+    BASE_URL
+  };
