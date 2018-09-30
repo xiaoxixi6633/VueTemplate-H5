a118deef7188fae2e1e4a9c8822ae64732a8a049
ex_mianmianwang@jieyuechina.com <281419583@qq.com>
2018-09-30 16:10:44 +0800

    net.js

--- src/dc/net.js
+++ src/dc/net.js
@@ -4,10 +4,8 @@
  * @Last Modified by: xixi
  * @Last Modified time: 2018-06-06 14:40:40
  */
-  // const BASE_URL = 'http://192.168.34.183:10000';//开发地址
-//  const BASE_URL = 'http://172.18.101.118:10000';//开发地址（内部开发和测试的地址）
-   const BASE_URL = 'http://172.18.101.111:10000';//测试地址（正式提测的地址）
-  // const BASE_URL = 'http://bi.jieyuechina.com/api'; //正式环境地址, #***###记得修改我的-关于-版本号
+
+   const BASE_URL = 'http://1dfkfd.111:10000';//测试地址（正式提测的地址）
   export default{
     BASE_URL
   };
